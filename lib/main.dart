4c3205d677a77988e3287937fbdaeee37e83ab30
Ahmed Muhammed <aaboelnor743@gmail.com>
2026-02-21 14:46:04 +0200

    -small edit import in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:movie/core/dpi/di.dart';
+import 'package:movie/core/di/di.dart';
+
 import 'package:movie/features/app_section/app_section.dart';
 import 'package:movie/features/details/presentation/view/details_screen.dart';
 
